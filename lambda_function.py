d79c1075944d490d422bccddf7fb7c643e0fc897
Stephen Carey <Stephen.Carey@iii.com>
2020-05-21 22:43:02 +0100

    Add Alexa lambda

---
+++ lambda_function.py
@@ -0,0 +1,231 @@
+"""
+AWS Lambda function to handle calls from alexa
+"""
+import logging
+import os
+
+
+from ask_sdk_model import Response
+from ask_sdk_core.handler_input import HandlerInput
+
+import ask_sdk_core.utils as ask_utils
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.skill_builder import CustomSkillBuilder
+
+from ask_sdk_s3.adapter import S3Adapter
+
+from irish_rail_timecheck import station
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+s3_adapter = S3Adapter(bucket_name=os.environ["S3_PERSISTENCE_BUCKET"])
+
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
+
+
+class CaptureStationIntentHandler(AbstractRequestHandler):
+    """Handler for save station Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("CaptureStationIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        slots = handler_input.request_envelope.request.intent.slots
+        depart = slots["homeStation"].value
+
+        local_station = {
+            "homeStation": depart
+        }
+
+        attributes_manager = handler_input.attributes_manager
+        attributes_manager.persistent_attributes = local_station
+        attributes_manager.save_persistent_attributes()
+
+        speak_output = 'Thanks, I will remember your local station is {depart}.' \
+            .format(depart=depart)
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .response
+        )
+
+
+class NextTrainIntentHandler(AbstractRequestHandler):
+    """Handler for Next Train where not information is stored."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("NextTrainIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        slots = handler_input.request_envelope.request.intent.slots
+        depart = slots["depart"].value
+        arrive = slots["arrive"].value
+
+        logging.info("Values sent were: %s, %s", depart, arrive)
+
+        speak_output = station.next_train_to(depart, arrive)
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .response
+        )
+
+
+class NextTrainToIntentHandler(AbstractRequestHandler):
+    """Handler for Next Train where not information is stored."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("NextTrainToIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        attr = handler_input.attributes_manager.persistent_attributes
+        departure = attr['homeStation']
+
+        slots = handler_input.request_envelope.request.intent.slots
+        destination = slots["destination"].value
+
+        logging.info("Values sent were: %s, %s", departure, destination)
+
+        speak_output = station.next_train_to(departure, destination)
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .response
+        )
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "You can say hello to me! How can I help?"
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Goodbye!"
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .response
+        )
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = "You just triggered " + intent_name + "."
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .response
+        )
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
+
+
+sb = CustomSkillBuilder(persistence_adapter=s3_adapter)
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(CaptureStationIntentHandler())
+sb.add_request_handler(NextTrainIntentHandler())
+sb.add_request_handler(NextTrainToIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+# make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+sb.add_request_handler(IntentReflectorHandler())
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+lambda_handler = sb.lambda_handler()
